aac2eeb1b697f28d0365c1f2a5c7418938667404
Erwan LBP <lebatardpo@eisti.eu>
2016-12-19 12:07:52 +0100

    add Game class which will contain the main function

---
+++ src/main/java/Game.java
@@ -0,0 +1,48 @@
+import org.w3c.dom.Document;
+import parse.InOut;
+
+import java.util.ArrayList;
+
+/**
+ * <h1>PACKAGE_NAME Game</h1>
+ *
+ * @author Erwan LBP
+ * @version 1.0
+ * @since 19-12-2016
+ */
+public class Game {
+    public static void main(String[] args) {
+
+        System.out.println("***** BEGIN *****");
+
+        if (args.length < 2) {
+            System.out.println("Not enough program arguments received");
+            System.exit(1);
+            return;
+        }
+
+        String mapXml = args[0];
+        Document mapDOM = InOut.loadMapXml(mapXml);
+        if (mapDOM == null) {
+            System.exit(1);
+            return;
+        }
+
+        String configTxt = args[1];
+        ArrayList<String> configContent = InOut.parseConfigTxt(args[1]);
+        if (configContent == null) {
+            System.exit(1);
+            return;
+        }
+
+        String outputXml = args[2];
+        if (outputXml == null) {
+            System.out.println("Output filename argument not found, will be : output.xml");
+            outputXml = "output.xml";
+        }
+
+
+
+        System.out.println("***** END *****");
+    }
+}
