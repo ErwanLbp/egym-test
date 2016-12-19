28f36e9408c10741b6bff56b3ec338522346fe60
Erwan LBP <lebatardpo@eisti.eu>
2016-12-19 14:37:43 +0100

    add Player class

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,44 @@
+package model;
+
+import log.Log;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <h1>model Player</h1>
+ *
+ * @author Erwan LBP
+ * @version 1.0
+ * @since 19-12-2016
+ */
+public class Player {
+
+    private Room location;
+    private Map<ObjectR, Boolean> todolist;
+
+    public Player(Room location, List<String> objectsToFind) {
+        this.location = location;
+        this.todolist = new HashMap<>();
+        for (String object : objectsToFind)
+            this.todolist.put(new ObjectR(object), false);
+        Log.info("Player start in room " + this.location + " and has to find : " + objectsToFind);
+    }
+
+    public Room getLocation() {
+        return location;
+    }
+
+    public void setLocation(Room location) {
+        this.location = location;
+    }
+
+    public boolean foundAnObject(ObjectR objectR) {
+        if (todolist.containsKey(objectR)) {
+            todolist.replace(objectR, true);
+            return true;
+        } else
+            return false;
+    }
+}
